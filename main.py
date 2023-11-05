21db66712496dd374599c48397784c0eec61fcb5
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2023-11-05 06:57:49 +0530

    created map.py

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 from taipy.gui import Gui
 from pages.login import page
 from pages.feed import frame
+# from pages.map import map_md
+
  
 pages = {
  
 "home": page,
 "feed": frame,
+# "Map":map_md,
+
  
 }
- 
-Gui(pages=pages).run(dark_mode=True)
+gui_multi_pages = Gui(pages=pages)
+
+if __name__ == '__main__':
+    gui_multi_pages.run(title="Clean Sight",
+                        use_reloader=True,
+                        port=5039)
